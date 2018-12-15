fa6ba28248b88be8d07ab82f116a5f0948c816fd
Cideg <34962099+Cideg@users.noreply.github.com>
2018-12-15 16:26:04 +0100

    Update CryptoNight_arm.h

--- xmrig/crypto/CryptoNight_arm.h
+++ xmrig/crypto/CryptoNight_arm.h
@@ -509,6 +509,43 @@ inline void cryptonight_single_hash(const uint8_t *__restrict__ input, size_t si
         al0 ^= cl;
         ah0 ^= ch;
         idx0 = al0;
+        
+        if (ALGO == xmrig::CRYPTONIGHT_ASC) {
+            uint64_t idx1; 
+            uint8_t* l1 = ctx0->long_state; 
+            uint64_t[2] c, c1; 
+            uint64_t* h1 = (uint64_t*)ctx0->hash_state; 
+            idx1 = h1[0] ^ h1[4]; 
+            ax1 = _mm_set_epi64x(h1[1] ^ h1[5], idx1); 
+            uint64_t c1l, c1h; 
+            uint64_t al1 = _mm_cvtsi128_si64(ax1); 
+            uint64_t ah1 = ((uint64_t*)&ax1)[1]; 
+            __m128i *ptr1;
+            for(size_t i = 0; i < 0x1000; i++)  //Change to 0x100000 after Softfork (20th December)
+            {
+	        __m128i c1x, c1xx; 
+	        ptr1 = (__m128i *)&l1[idx1 & 0x1FFFF0]; 
+	        c1x = _mm_load_si128(ptr1); 
+	        if(SOFT_AES) c1x = soft_aesenc(c1x, ax1); else c1x = _mm_aesenc_si128(c1x, ax1); 
+	        _mm_store_si128(R128(c), c1x);  
+	        _mm_store_si128(R128(c1), _mm_load_si128(ptr1); 
+	        ptr1 = state_index(c); 
+	        c[0] ^= al1; c[1] ^= ah1; 
+	        _mm_store_si128((__m128i *)ptr1, _mm_load_si128(c)); 
+	        ptr1 = state_index(c);
+	        _mm_store_si128((__m128i *)ptr1, _mm_load_si128(c1)); 
+	        c1[0] ^= c[0]; c1[1] ^= c[1];
+	        ptr1 = state_index(c1);
+	        _mm_store_si128((__m128i *)ptr1, _mm_load_si128(c1)); 
+	        c1l = ((uint64_t*)ptr1)[0]; 
+	        c1h = ((uint64_t*)ptr1)[1]; 
+	        ((uint64_t*)ptr1)[0] = al1; 
+	        ((uint64_t*)ptr1)[1] = ah1; 
+	        al1 ^= c1l; 
+	        ah1 ^= c1h; 
+	        ax1 = _mm_set_epi64x(ah1, al1); 
+	        idx1 = al1;
+    }
 
         if (ALGO == xmrig::CRYPTONIGHT_HEAVY) {
             const int64x2_t x = vld1q_s64(reinterpret_cast<const int64_t *>(&l0[idx0 & MASK]));
@@ -640,6 +677,79 @@ inline void cryptonight_double_hash(const uint8_t *__restrict__ input, size_t si
         al0 ^= cl;
         ah0 ^= ch;
         idx0 = al0;
+
+        if (ALGO == xmrig::CRYPTONIGHT_ASC) {
+            __m128i ax0, ax1; 
+	        uint64_t idx0, idx1; 
+	        uint8_t* l0 = ctx0->long_state; 
+	        uint8_t* l1 = ctx1->long_state; 
+	        uint64_t* h0 = (uint64_t*)ctx0->hash_state; 
+	        uint64_t* h1 = (uint64_t*)ctx1->hash_state; 
+	        idx0 = h0[0] ^ h0[4]; 
+	        idx1 = h1[0] ^ h1[4]; 
+	        ax0 = _mm_set_epi64x(h0[1] ^ h0[5], idx0); 
+	        ax1 = _mm_set_epi64x(h1[1] ^ h1[5], idx1); 
+	        __m128i *ptr0, *ptr1;
+	    for(size_t i = 0; i < 0x1000; i++)
+	    {
+		    __m128i c0x, c0xx, c1x, c1xx; 
+		    ptr0 = (__m128i *)&l0[idx0 & 0x1FFFF0]; 
+		    ptr1 = (__m128i *)&l1[idx1 & 0x1FFFF0]; 
+		    c0x = _mm_load_si128(ptr0); 
+		    c1x = _mm_load_si128(ptr1); 
+		    c0xx = _mm_load_si128(ptr0); 
+		    c1xx = _mm_load_si128(ptr1); 
+		    if(SOFT_AES){
+			c0x = soft_aesenc(c0x, ax0);
+			c1x = soft_aesenc(c1x, ax1);}
+        else{ 
+			c0x = _mm_aesenc_si128(c0x, ax0);
+			c1x = _mm_aesenc_si128(c1x, ax1);}
+		    _mm_store_si128((__m128i *)ptr0, c0x); 
+		    _mm_store_si128((__m128i *)ptr1, c1x); 
+		    idx0 = _mm_cvtsi128_si64(c0x); 
+		    idx1 = _mm_cvtsi128_si64(c1x); 
+		    ptr0 = (__m128i *)&l0[idx0 & 0x1FFFF0]; 
+		    ptr1 = (__m128i *)&l1[idx1 & 0x1FFFF0]; 
+		    if(PREFETCH) {
+			_mm_prefetch((const char*)ptr0, _MM_HINT_T0);
+			_mm_prefetch((const char*)ptr1, _MM_HINT_T0);} 
+	    	_mm_store_si128((__m128i *)ptr0, c0xx); 
+	    	_mm_store_si128((__m128i *)ptr1, c1xx); 
+		    c0xx = _mm_xor_si128(c0xx,c0x);
+		    c1xx = _mm_xor_si128(c1xx,c1x);
+		    idx0 = _mm_cvtsi128_si64(c0xx); 
+		    idx1 = _mm_cvtsi128_si64(c1xx); 
+		    ptr0 = (__m128i *)&l0[idx0 & 0x1FFFF0]; 
+		    ptr1 = (__m128i *)&l1[idx1 & 0x1FFFF0]; 
+		    _mm_store_si128((__m128i *)ptr0, c0xx); 
+		    _mm_store_si128((__m128i *)ptr1, c1xx); 
+		    uint64_t c0l, c0h; 
+		    uint64_t c1l, c1h; 
+		    uint64_t al0 = _mm_cvtsi128_si64(ax0); 
+		    uint64_t al1 = _mm_cvtsi128_si64(ax1); 
+		    uint64_t ah0 = ((uint64_t*)&ax0)[1]; 
+		    uint64_t ah1 = ((uint64_t*)&ax1)[1]; 
+		    c0l = ((uint64_t*)ptr0)[0]; 
+		    c1l = ((uint64_t*)ptr1)[0]; 
+		    c0h = ((uint64_t*)ptr0)[1]; 
+		    c1h = ((uint64_t*)ptr1)[1]; 
+		    ((uint64_t*)ptr0)[0] = al0; 
+		    ((uint64_t*)ptr1)[0] = al1; 
+		    if(PREFETCH) {
+			_mm_prefetch((const char*)ptr0, _MM_HINT_T0);
+			_mm_prefetch((const char*)ptr1, _MM_HINT_T0);}
+		    ((uint64_t*)ptr0)[1] = ah0; 
+		    ((uint64_t*)ptr1)[1] = ah1; 
+		    al0 ^= c0l; 
+		    al1 ^= c1l; 
+		    ah0 ^= c0h; 
+		    ah1 ^= c1h; 
+		    ax0 = _mm_set_epi64x(ah0, al0); 
+		    ax1 = _mm_set_epi64x(ah1, al1); 
+		    idx0 = al0;
+		    idx1 = al1;            
+
 
         if (ALGO == xmrig::CRYPTONIGHT_HEAVY) {           
             const int64x2_t x = vld1q_s64(reinterpret_cast<const int64_t *>(&l0[idx0 & MASK]));
